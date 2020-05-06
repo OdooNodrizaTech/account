6ce057406a0388b991af4aacec43e2771726aea8
Informatica Arelux <informatica@arelux.com>
2020-05-06 13:18:24 +0200

    v12 account_invoice_mail_followers_extra

---
+++ account_invoice_mail_followers_extra/models/account_invoice_mail_followers_extra.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models, fields
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountInvoiceMailFollowersExtra(models.Model):
+    _name = 'account_invoice_mail_followers_extra'
+    
+    partner_id = fields.Many2one(
+        comodel_name='res.partner', 
+        domain=[('customer', '=', True)],
+        string='Cliente',
+    )
+    partner_ids_extra = fields.Many2many(
+        comodel_name='res.partner', 
+        string='Seguidores adicionales facturas'
+    )                                                                        
