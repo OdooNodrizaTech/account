e6788dacadbebe74c2484892aa2602f0b503b85e
Informatica Arelux <informatica@arelux.com>
2020-05-19 08:46:17 +0200

    mejoras

--- account_invoice_shipping_expedition/models/account_invoice.py
+++ account_invoice_shipping_expedition/models/account_invoice.py
@@ -3,6 +3,7 @@ import logging
 _logger = logging.getLogger(__name__)
 
 from odoo import api, models, fields
+import base64
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
@@ -16,6 +17,87 @@ class AccountInvoice(models.Model):
         stored=False
     )
     shipping_expedition_ids = fields.One2many('shipping.expedition', 'account_invoice_id', string='Shipping Expedition Ids', readonly=True)
+
+    @api.one
+    def shipping_expedition_datas_lines_process(self, carrier_type, lines):
+        auto_create_new_shipping_expedition = False
+        if len(lines)>0:
+            delivery_codes = []
+            origins = []
+            for line_key in lines:
+                line = lines[line_key]
+                delivery_codes.append(str(line['delivery_code']))
+            #search all
+            shipping_expedition_ids = self.env['shipping.expedition'].sudo().search(
+                [
+                    ('delivery_code', 'in', delivery_codes),
+                    ('carrier_id.carrier_type', '=', carrier_type)
+                ]
+            )
+            if len(shipping_expedition_ids)>0:
+                for shipping_expedition_id in shipping_expedition_ids:
+                    line = lines[str(shipping_expedition_id.delivery_code)]
+                    #update
+                    shipping_expedition_id.account_invoice_id = self.id
+                    shipping_expedition_id.invoice_date = self.date_invoice
+                    shipping_expedition_id.currency_id = self.currency_id.id
+                    shipping_expedition_id.cost = line['cost']
+                    #state
+                    if shipping_expedition_id.state!='delivered':
+                        shipping_expedition_id.state = 'delivered'
+                    #remove
+                    del lines[str(shipping_expedition_id.delivery_code)]
+            #auto-create
+            if auto_create_new_shipping_expedition==True and len(lines)>0:
+                _logger.info('Faltarian por crear ' + str(len(lines)) + ' expediciones que nos han facturado y no teniamos')
+                lines_old = lines
+                lines = {}
+                origins = []
+                for line_key_old in lines_old:
+                    line_old = lines_old[line_key_old]
+                    lines[str(line_old['origin'])] = line_old
+                    origins.append(str(line_old['origin']))
+
+                stock_picking_ids = self.env['stock.picking'].sudo().search(
+                    [
+                        ('name', 'in', origins),
+                        ('carrier_id.carrier_type', '=', carrier_type)
+                    ]
+                )
+                _logger.info(len(stock_picking_ids))
+                if len(stock_picking_ids)>0:
+                    for stock_picking_id in stock_picking_ids:
+                        line = lines[str(stock_picking_id.name)]
+                        shipping_expedition_vals = {
+                            'picking_id': stock_picking_id.id,
+                            'partner_id': stock_picking_id.partner_id.id,
+                            'delivery_code': line['delivery_code'],
+                            'date': line['date'],
+                            'origin': stock_picking_id.name,
+                            'carrier_id': stock_picking_id.carrier_id.id,
+                            'account_invoice_id': self.id,
+                            'invoice_date': self.date_invoice,
+                            'currency_id': self.currency_id.id,
+                            'cost': line['cost'],
+                            'state': 'delivered',
+                        }
+                        res_partner_obj = self.env['shipping.expedition'].create(shipping_expedition_vals)
+
+    @api.one
+    def shipping_expedition_datas_override(self, file_encoded):
+        return False
+
+    @api.one
+    def write(self, vals):
+        if 'shipping_expedition_datas' in vals:
+            if vals['shipping_expedition_datas'] != False:
+                if self.is_supplier_delivery_carrier == True:
+                    file_encoded = base64.b64decode(vals['shipping_expedition_datas'])
+                    self.shipping_expedition_datas_override(file_encoded)
+            # remove
+            del vals['shipping_expedition_datas']
+            # write
+        return super(AccountInvoice, self).write(vals)
 
     @api.one
     def _get_is_supplier_delivery_carrier(self):
--- account_invoice_shipping_expedition_nacex/models/account_invoice.py
+++ account_invoice_shipping_expedition_nacex/models/account_invoice.py
@@ -4,7 +4,6 @@ _logger = logging.getLogger(__name__)
 
 from odoo import api, models, fields
 from odoo.exceptions import Warning
-import base64
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
@@ -15,69 +14,55 @@ class AccountInvoice(models.Model):
     )
 
     @api.one
-    def generate_invoice_line_ids_custom(self, supplier_lines):
-        return super(AccountInvoice, self).generate_invoice_line_ids_custom(supplier_lines)
-
-    @api.one
-    def write(self, vals):
-        if 'shipping_expedition_datas' in vals:
-            if vals['shipping_expedition_datas']!=False:
-                if self.is_supplier_delivery_carrier == True:
-                    delivery_carrier_id = self._get_delivery_carrier_filter_partner_id()[0]
-                    if delivery_carrier_id.carrier_type=='nacex':
-                        if len(self.invoice_line_ids)>0:
-                            raise Warning("Es necesario que no haya lineas definidas previamente")
-                        else:
-                            supplier_lines = []
-                            file_encoded = base64.b64decode(vals['shipping_expedition_datas'])
-                            file_encoded_split = file_encoded.split('\n')
-                            if len(file_encoded_split)>1:
-                                line = 1
-                                for file_encoded_line in file_encoded_split:
-                                    line_data = file_encoded_line.split(';')
-                                    if len(line_data)>1:#Skip last line empty
-                                        #remove ' character
-                                        item_count = 0
-                                        for line_data_item in line_data:
-                                            line_data[item_count] = line_data_item[1:-1]
-                                            item_count += 1
-                                        #operations
-                                        if line>1:
-                                            #data
-                                            key_0 = line_data[0]
-                                            tipo = line_data[1]
-                                            if key_0=='1' and tipo=='FRE':
-                                                num_factura = line_data[2]
-                                                departamento = line_data[3]
-                                                albaran = line_data[4]
-                                                cost = line_data[22].replace(',', '.')
-                                                #ooperations
-                                                if departamento!='ONLINE':
-                                                    _logger.info('NO es Online (RARO)')
-                                                else:
-                                                    if num_factura!=self.reference:
-                                                        _logger.info('El n factura de la linea no coincide con el de la factura')
-                                                    else:
-                                                        #search_stock_picking
-                                                        shipping_expedition_ids = self.env['shipping.expedition'].sudo().search(
-                                                            [
-                                                                ('delivery_code', '=', albaran),
-                                                                ('carrier_id.carrier_type', '=', 'nacex')
-                                                            ]
-                                                        )
-                                                        if len(shipping_expedition_ids)==0:
-                                                            _logger.info('Raro, la referencia '+str(albaran)+' no la tenemos y nos la facturan')
-                                                        else:
-                                                            shipping_expedition_id = shipping_expedition_ids[0]
-                                                            _logger.info('Actualizamos respecto a la expedicion ' + str(shipping_expedition_id.id))
-                                                            #update
-                                                            shipping_expedition_id.account_invoice_id = self.id
-                                                            shipping_expedition_id.invoice_date = self.date_invoice
-                                                            shipping_expedition_id.currency_id = self.currency_id.id
-                                                            shipping_expedition_id.cost = cost
-                                    #line
-                                    line += 1
-            #remove
-            del vals['shipping_expedition_datas']
-        # write
-        return super(AccountInvoice, self).write(vals)
+    def shipping_expedition_datas_override(self, file_encoded):
+        return_action = super(AccountInvoice, self).shipping_expedition_datas_override(file_encoded)
+        #operations
+        delivery_carrier_id = self._get_delivery_carrier_filter_partner_id()[0]
+        if delivery_carrier_id.carrier_type == 'nacex':
+            lines = {}
+            file_encoded_split = file_encoded.split('\n')
+            if len(file_encoded_split) > 1:
+                line = 1
+                for file_encoded_line in file_encoded_split:
+                    line_data = file_encoded_line.split(';')
+                    if len(line_data) > 1:  # Skip last line empty
+                        # remove ' character
+                        item_count = 0
+                        for line_data_item in line_data:
+                            line_data[item_count] = line_data_item[1:-1]
+                            item_count += 1
+                        # operations
+                        if line > 1:
+                            # data
+                            key_0 = line_data[0]
+                            tipo = line_data[1]
+                            if key_0 == '1' and tipo == 'FRE':
+                                num_factura = line_data[2]
+                                departamento = line_data[3]
+                                albaran = line_data[4]
+                                #fecha_albaran
+                                fecha_albaran = line_data[5]
+                                fecha_albaran_split = fecha_albaran.split('/')
+                                fecha_albaran = fecha_albaran_split[2]+'-'+fecha_albaran_split[1]+'-'+fecha_albaran_split[0]
+                                #others
+                                referencia = line_data[6]
+                                cost = line_data[22].replace(',', '.')
+                                # ooperations
+                                if departamento != 'ONLINE':
+                                    _logger.info('NO es Online (RARO)')
+                                else:
+                                    if num_factura != self.reference:
+                                        _logger.info('El n factura de la linea no coincide con el de la factura')
+                                    else:
+                                        lines[albaran] = {
+                                            'delivery_code': albaran,
+                                            'origin': referencia,
+                                            'date': fecha_albaran,
+                                            'cost': cost
+                                        }
+                    # line
+                    line += 1
+            #shipping_expedition_datas_lines_process
+            super(AccountInvoice, self).shipping_expedition_datas_lines_process('nacex', lines)
+        #return
+        return return_action
