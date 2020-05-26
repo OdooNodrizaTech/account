f4a3ca67830b1caf5fe646d7b6b6c654d43cab47
Informatica Arelux <informatica@arelux.com>
2020-05-26 12:07:44 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-779

--- ont_base_account/models/account_payment_mode.py
+++ ont_base_account/models/account_payment_mode.py
@@ -12,3 +12,8 @@ class AccountPaymentMode(models.Model):
     position = fields.Integer(
         string='Posicion'
     )
+    show_payment_method_id_journal_ids_in_pdf = fields.Boolean(
+        string='Mostrar en PDF',
+        help='Mostrar los numeros de cuenta de el/los diario/s del metodo de pago vinculado en los PDFs (Presupuesto y Factura)',
+        default=False
+    )
