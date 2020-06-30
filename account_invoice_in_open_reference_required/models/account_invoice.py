a12a5e071bd4b37337c949d970ae45f98ae3c4c4
Informatica Arelux <informatica@arelux.com>
2020-06-30 08:31:01 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-882

---
+++ account_invoice_in_open_reference_required/models/account_invoice.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models, fields
+from odoo.exceptions import Warning
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_open(self):
+        allow_confirm = True
+        # check
+        for obj in self:
+            if obj.type == "in_invoice" and obj.reference == False:
+                allow_confirm = False
+                raise Warning("Es necesario definir una referencia de proveedor para validar la factura de compra")
+        # allow_confirm
+        if allow_confirm == True:
+            return super(AccountInvoice, self).action_invoice_open()
---
+++ account_invoice_open_partner_vat_required/models/account_invoice.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models, fields
+from odoo.exceptions import Warning
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_open(self):
+        allow_confirm = True
+        # check
+        for obj in self:
+            if obj.partner_id.vat == False:
+                allow_confirm = False
+                raise Warning("Es necesario definir un CIF/NIF para el cliente de la factura")
+        # allow_confirm
+        if allow_confirm == True:
+            return super(AccountInvoice, self).action_invoice_open()
