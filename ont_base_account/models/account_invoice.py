7186d0dc0a607a2fa9a52388855469315f6b0717
Informatica Arelux <informatica@arelux.com>
2020-06-30 08:27:15 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-882

--- ont_base_account/models/account_invoice.py
+++ ont_base_account/models/account_invoice.py
@@ -17,21 +17,6 @@ class AccountInvoice(models.Model):
         string='Banco'
     )
     
-    @api.multi
-    def action_invoice_open(self):
-        allow_confirm = True
-        #check
-        for obj in self:
-            if obj.partner_id.vat==False:
-                allow_confirm = False
-                raise Warning("Es necesario definir un CIF/NIF para el cliente de la factura.\n")
-            elif obj.type=="in_invoice" and obj.reference==False:
-                allow_confirm = False
-                raise Warning("Es necesario definir una referencia de proveedor para validar la factura de compra.\n")
-        #allow_confirm
-        if allow_confirm==True:
-            return super(AccountInvoice, self).action_invoice_open()        
-    
     @api.multi        
     def _partner_bank_name(self):
         for obj in self:
@@ -40,4 +25,4 @@ class AccountInvoice(models.Model):
                 if obj.partner_bank_id.bank_id.id>0:
                     obj.partner_bank_name = obj.partner_bank_id.bank_id.name + ' ' + obj.partner_bank_id.acc_number[-4:]
                 else:
-                    obj.partner_bank_name = obj.partner_bank_id.acc_number                                                                                                                                                                                                          
+                    obj.partner_bank_name = obj.partner_bank_id.acc_number
