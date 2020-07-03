8f5617e1728e4bc3e4d2f51985a3c070dcd7e125
Informatica Arelux <informatica@arelux.com>
2020-07-03 08:49:49 +0200

    # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).

--- account_banking_mandate_auto_create/models/__init__.py
+++ account_banking_mandate_auto_create/models/__init__.py
@@ -1,2 +1,4 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import account_banking_mandate
 import res_partner_bank
--- account_payment_order_invoice/models/__init__.py
+++ account_payment_order_invoice/models/__init__.py
@@ -1,2 +1,4 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import account_payment_order
 import res_bank
