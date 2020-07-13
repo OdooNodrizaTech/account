a27603854d71116dd08a53342663a91cfda85b9a
victoralmau <victoralmau@gmail.com>
2020-07-13 09:50:15 +0200

    fix

--- account_invoice_valid_auto_paid/__manifest__.py
+++ account_invoice_valid_auto_paid/__manifest__.py
@@ -1,14 +1,18 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    'name': 'Account Invoice Valida Auto Paid',
-    'version': '12.0.1.0.0',    
-    'author': 'Odoo Nodriza Tech (ONT)',
-    'website': 'https://nodrizatech.com/',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'depends': ['base', 'account', 'payment'],
-    'data': [],
-    'installable': True,
-    'auto_install': False,    
+    "name": "Account Invoice Valida Auto Paid",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://nodrizatech.com/",
+    "author": "Odoo Nodriza Tech (ONT), "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "payment"
+    ],
+    "data": [],
+    "installable": True,
 }
