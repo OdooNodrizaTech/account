4f06905bd1fc358ef8526285c07f76fb0865b8cc
victoralmau <victoralmau@gmail.com>
2020-07-14 08:40:46 +0200

    reestructuración

--- ont_base_account/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import account_asset
-from . import account_bank_statement_line
-from . import account_fiscal_position
-from . import account_invoice
-from . import account_payment_mode
-from . import account_payment_term
-from . import sale_order 
