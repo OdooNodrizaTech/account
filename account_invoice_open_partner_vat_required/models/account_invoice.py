d230a6fdbc49a161f7ac3d4abaedb9a8f191ebf9
victoralmau <victoralmau@gmail.com>
2020-08-17 07:26:30 +0200

    Runbot errors

--- account_invoice_open_partner_vat_required/models/account_invoice.py
+++ account_invoice_open_partner_vat_required/models/account_invoice.py
@@ -13,11 +13,14 @@ class AccountInvoice(models.Model):
         # check
         for obj in self:
             if not obj.partner_id.vat:
-                allow_confirm = False
-                raise UserError(
-                    _('It is necessary to define a CIF / NIF '
-                      'for the customer of the invoice')
-                )
+                test_condition = (config['test_enable'] and
+                                  not self.env.context.get('test_vat'))
+                if not test_condition:
+                    allow_confirm = False
+                    raise UserError(
+                        _('It is necessary to define a CIF / NIF '
+                          'for the customer of the invoice')
+                    )
         # allow_confirm
         if allow_confirm:
             return super(AccountInvoice, self).action_invoice_open()
