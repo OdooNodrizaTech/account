956433cfeeed0792ffcac65a36e7896a122115c6
victoralmau <victoralmau@gmail.com>
2020-08-17 07:29:53 +0200

    Update account_invoice.py

--- account_invoice_open_partner_vat_required/models/account_invoice.py
+++ account_invoice_open_partner_vat_required/models/account_invoice.py
@@ -1,6 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
+from odoo.tools import config
 from odoo.exceptions import Warning as UserError
 
 
