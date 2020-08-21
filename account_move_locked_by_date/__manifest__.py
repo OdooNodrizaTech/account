80a2be8a503793aeda52be82ecc35ec34b1ac7fd
victoralmau <victoralmau@gmail.com>
2020-08-21 12:57:21 +0200

    account_move_locked_by_date

---
+++ account_move_locked_by_date/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account move locked by date",
+    "version": "12.0.1.0.0",
+    "author": "Odoo Nodriza Tech (ONT), "
+              "Odoo Community Association (OCA)",
+    "website": "https://nodrizatech.com/",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "data/ir_configparameter_data.xml",
+    ],
+    "installable": True
+}
---
+++ account_move_locked_by_date/models/account_move.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.multi
+    def _check_lock_date(self):
+        res = super()._check_lock_date()
+        lock_to_date = str(self.env['ir.config_parameter'].sudo().get_param(
+            'account_locked_by_date_limit')
+        )
+        for move in self:
+            if move.date <= lock_to_date:
+                message = _("No puedes crear/Modificar asientos con "
+                            "fecha anterior a %s") % (lock_to_date)
+                raise UserError(message)
+        return res
