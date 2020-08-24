f7cc479cf7418bce59df130ff8d31eb621a34e37
victoralmau <victoralmau@gmail.com>
2020-08-24 13:08:12 +0200

    account_move_locked_by_date

---
+++ account_move_locked_by_date/models/account_move.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, models, _
+from datetime import datetime
+from odoo.exceptions import UserError
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.multi
+    def _check_lock_date(self):
+        res = super(AccountMove, self)._check_lock_date()
+        lock_to_date = str(self.env['ir.config_parameter'].sudo().get_param(
+            'account_locked_by_date_limit')
+        )
+        for move in self:
+            if move.date <= lock_to_date:
+                message = _("No puedes crear/Modificar asientos con "
+                            "fecha anterior a %s (Fecha asiento: %s)") % (
+                    lock_to_date, move.date
+                )
+                raise UserError(message)
+        return res
