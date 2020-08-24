fbe8826e2f80409f2d053d117c63a921dddfd9d4
victoralmau <victoralmau@gmail.com>
2020-08-24 13:20:29 +0200

    Update account_move.py

--- account_move_locked_by_date/models/account_move.py
+++ account_move_locked_by_date/models/account_move.py
@@ -13,10 +13,11 @@ class AccountMove(models.Model):
         lock_to_date = str(self.env['ir.config_parameter'].sudo().get_param(
             'account_locked_by_date_limit')
         )
-        lock_to_date = datetime.strptime(lock_to_date, "%Y-%m-%d").date()
         for move in self:
             if move.date <= lock_to_date:
                 message = _("No puedes crear/Modificar asientos con "
-                            "fecha anterior a %s") % (lock_to_date)
+                            "fecha anterior a %s (Fecha asiento: %s)") % (
+                    lock_to_date, move.date
+                )
                 raise UserError(message)
         return res
